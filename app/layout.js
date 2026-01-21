870b2906f83e5d64c2ee587d99daad534e7dd17b
ShinSangEon <new5268@naver.com>
2026-01-21 19:45:10 +0900

    chore: setup GTM and initial config

--- app/layout.js
+++ app/layout.js
@@ -32,6 +32,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <head>
+        {/* Google Tag Manager */}
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`,
+            }}
+          />
+        )}
         <link
           rel="stylesheet"
           as="style"
@@ -40,6 +52,17 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="antialiased font-pretendard">
+        {/* Google Tag Manager (noscript) */}
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+        )}
         {/* Google AdSense Script */}
         {process.env.NEXT_PUBLIC_ADSENSE_ID && (
           <script
